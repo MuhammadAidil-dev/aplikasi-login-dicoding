87c9f26248afb1fda452e06e10558fb63c57bf08
Muhammad Aidil <muhammad.aidil09767@gmail.com>
2024-09-09 22:41:12 +0700

    Add fitur logout (#2)
    * update apllikasi add fitur logout
    
    menambahkan fitur logout pada aplikasi login
    
    * Delete Aplikasi-Login-01-starter-study-case directory
    
    menghapus directori aplikasi login sebelumnya

--- Aplikasi-Login-02-solution-study-case/assets/scripts/login-script.js
+++ Aplikasi-Login-02-solution-study-case/assets/scripts/login-script.js
@@ -13,18 +13,13 @@ const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  	const email = inputEmailElement.value;
-  	const password = inputPasswordElement.value;
+  event.preventDefault();
 
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
-  	if(email === expectedEmail && password === expectedPassword){
-  		goToHome();
-  	} else {
-  		showPopUp();
-  	}
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+  if (email == expectedEmail && password == expectedPassword) {
+    goToHome();
+  } else {
+    showPopUp();
+  }
 });
